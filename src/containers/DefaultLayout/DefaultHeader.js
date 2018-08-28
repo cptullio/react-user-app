cdc63920619210463a4cd2f9e600aa9db6dc959f
Carlos Muniz <carlos.muniz@checkmarx.com>
2018-08-28 01:57:45 +0100

    Adding CoreUI.

---
+++ src/containers/DefaultLayout/DefaultHeader.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import { Badge, DropdownItem, DropdownMenu, DropdownToggle, Nav, NavItem, NavLink } from 'reactstrap';
+import PropTypes from 'prop-types';
+
+import { AppAsideToggler, AppHeaderDropdown, AppNavbarBrand, AppSidebarToggler } from '@coreui/react';
+
+const propTypes = {
+  children: PropTypes.node,
+};
+
+const defaultProps = {};
+
+class DefaultHeader extends Component {
+  render() {
+
+    // eslint-disable-next-line
+    const { children, ...attributes } = this.props;
+
+    return (
+      <React.Fragment>
+        <AppSidebarToggler className="d-lg-none" display="md" mobile />
+        <a  class="navbar-brand">
+          <span class="navbar-brand-full" width="89" height="25">My Experiment</span>  
+          <span class="navbar-brand-minimized" width="30" height="30">MyE</span>
+        </a>
+
+        <AppSidebarToggler className="d-md-down-none" display="lg" />
+
+        <Nav className="ml-auto" navbar>
+          <AppHeaderDropdown direction="down">
+            <DropdownToggle nav>
+              <img src={'assets/img/avatars/6.jpg'} className="img-avatar" alt="admin@bootstrapmaster.com" />
+            </DropdownToggle>
+            <DropdownMenu right style={{ right: 'auto' }}>
+              <DropdownItem><i className="fa fa-file"></i> Statistics</DropdownItem>
+              <DropdownItem><i className="fa fa-lock"></i> Logout</DropdownItem>
+            </DropdownMenu>
+          </AppHeaderDropdown>
+        </Nav>
+      </React.Fragment>
+    );
+  }
+}
+
+DefaultHeader.propTypes = propTypes;
+DefaultHeader.defaultProps = defaultProps;
+
+export default DefaultHeader;
